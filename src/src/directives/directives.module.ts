f8020161dd198cab9f0c899f84b9711ec68873d4
Brazilian114 <koravit114@gmail.com>
2018-10-03 02:45:37 +0700

    first commit

--- src/src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ParallaxDirective } from './parallax/parallax';
-@NgModule({
-	declarations: [ParallaxDirective],
-	imports: [],
-	exports: [ParallaxDirective]
-})
-export class DirectivesModule {}
